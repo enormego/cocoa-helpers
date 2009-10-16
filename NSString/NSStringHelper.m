fc6aa5ce04dd80703c30596eedd81710bc90f19f
shnhrrsn <shnhrrsn@gmail.com>
2009-10-16 10:28:35 -0400

    Added license to all of the headers/readme

--- NSString/NSStringHelper.m
+++ NSString/NSStringHelper.m
@@ -5,6 +5,10 @@
 //  Created by Shaun Harrison on 10/14/08.
 //  Copyright 2008-2009 enormego. All rights reserved.
 //
+// This work is licensed under the Creative Commons GNU General Public License License.
+// To view a copy of this license, visit http://creativecommons.org/licenses/GPL/2.0/
+// or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+//
 
 #import "NSStringHelper.h"
 #import <CommonCrypto/CommonDigest.h>
